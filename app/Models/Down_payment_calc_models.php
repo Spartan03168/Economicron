5a85121120c481c35c47e63959b18faea26352ea
Tommaso Amorosi <tomy.bawse@gmail.com>
2024-11-14 09:42:16 +0100

    Initial commit.

---
+++ app/Models/Down_payment_calc_models.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Down_payment_calc_models extends Model
+{
+    //
+}
---
+++ app/Models/Home_page_model.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Home_page_model extends Model
+{
+    //
+}
