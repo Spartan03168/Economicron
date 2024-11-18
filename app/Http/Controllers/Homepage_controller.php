30359deaaa3483100235a962865d6493db5941dd
Tommaso Amorosi <tomy.bawse@gmail.com>
2024-11-18 13:47:56 +0100

    Initial first drafts of first 2 operational controllers.

--- app/Http/Controllers/Homepage_controller.php
+++ app/Http/Controllers/Homepage_controller.php
@@ -4,11 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Homepage_controller extends Controller
-{
-    public function index()
-    {
+class Homepage_controller extends Controller{
+    public function index(){
         // Page rendering
         return view("homepage");
+        }
     }
-}
--- app/Http/Controllers/Routing_page_controller.php
+++ app/Http/Controllers/Routing_page_controller.php
@@ -4,7 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Routing_page_controller extends Controller
-{
-    //
-}
+class Routing_page_controller extends Controller{
+    //Routing page
+    public function index(){
+        return view('routing_page');
+        }
+    /**
+     * Routing paths
+     */
+    public function routing_path_saving_calculations(){
+        return redirect()->route("Savings_math.index");
+        }
+
+    public function routing_path_tax_math(){
+        return redirect()->route("tax_deduction_calculation.index");
+        }
+
+    public function routing_path_compound_interest(){
+        return redirect()->route("Compound_interest.index");
+        }
+
+    public function routing_path_down_payment(){
+        return redirect()->route("down_payment_calculation.index");
+        }
+
+    }
